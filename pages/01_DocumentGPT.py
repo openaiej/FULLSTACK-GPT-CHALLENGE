db456987647290e089b030a53e5feb162645289d
MaEunJa <ttochi0115@gmail.com>
2025-02-19 01:34:10 +0900

    d

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,5 +1,5 @@
 from langchain.prompts import ChatPromptTemplate
-from langchain.document_loaders import UnstructuredFileLoader
+from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
